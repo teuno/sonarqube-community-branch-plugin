1f0ad5334d32521a0fcad05c0a53bdf2d0e2f147
thooijer <thooijer@quintor.nl>
2020-07-13 12:05:15 +0200

    wrote test for IssueFilterRunner. cleanup still needed.

--- src/main/java/com/github/mc1arke/sonarqube/plugin/ce/pullrequest/commentfilter/IssueFilterRunner.java
+++ src/main/java/com/github/mc1arke/sonarqube/plugin/ce/pullrequest/commentfilter/IssueFilterRunner.java
@@ -1,9 +1,6 @@
 package com.github.mc1arke.sonarqube.plugin.ce.pullrequest.commentfilter;
 
 import com.github.mc1arke.sonarqube.plugin.ce.pullrequest.PostAnalysisIssueVisitor;
-import com.github.mc1arke.sonarqube.plugin.ce.pullrequest.commentfilter.SeverityComparator;
-import com.github.mc1arke.sonarqube.plugin.ce.pullrequest.commentfilter.TypeComparator;
-import org.sonar.api.rule.Severity;
 
 import java.util.List;
 import java.util.function.Predicate;
@@ -11,23 +8,41 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class IssueFilterRunner {
-
     private List<Predicate<PostAnalysisIssueVisitor.ComponentIssue>> filters;
     private Integer maxAmountOfIssues;
-
-    public IssueFilterRunner(List<Predicate<PostAnalysisIssueVisitor.ComponentIssue>> filters) {
-        this.filters = filters;
-    }
+    private SeverityComparator severityComparator;
+    private TypeComparator typeComparator;
 
     public IssueFilterRunner(List<Predicate<PostAnalysisIssueVisitor.ComponentIssue>> filters, Integer maxAmountOfIssues) {
         this.filters = filters;
         this.maxAmountOfIssues = maxAmountOfIssues;
     }
 
+    public IssueFilterRunner(
+            List<Predicate<PostAnalysisIssueVisitor.ComponentIssue>> filters,
+            SeverityComparator severityComparator,
+            TypeComparator typeComparator) {
+        this.filters = filters;
+        this.severityComparator = severityComparator;
+        this.typeComparator = typeComparator;
+    }
+
+    public IssueFilterRunner(
+            List<Predicate<PostAnalysisIssueVisitor.ComponentIssue>> filters, Integer maxAmountOfIssues,
+            SeverityComparator severityComparator,
+            TypeComparator typeComparator) {
+        this.filters = filters;
+        this.maxAmountOfIssues = maxAmountOfIssues;
+        this.severityComparator = severityComparator;
+        this.typeComparator = typeComparator;
+    }
+
     public List<PostAnalysisIssueVisitor.ComponentIssue> filterIssues(List<PostAnalysisIssueVisitor.ComponentIssue> issues) {
         Stream<PostAnalysisIssueVisitor.ComponentIssue> stream = issues.stream()
-                .filter(filters.stream().reduce(issue -> true, Predicate::and))
-                .sorted(new SeverityComparator().thenComparing(new TypeComparator()));
+                .filter(filters.stream()
+                        .reduce(issue -> true,
+                                Predicate::and))
+                .sorted(severityComparator.thenComparing(typeComparator));
 
         if (maxAmountOfIssues != null && maxAmountOfIssues > 0) stream = stream.limit(maxAmountOfIssues);
 
